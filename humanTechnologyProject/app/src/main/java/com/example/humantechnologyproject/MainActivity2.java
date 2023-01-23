420039d31843f309b20d3aa9ef6d3d41afa64dec
AAusa <aa21.aausaberri87@iespabloserrano.com>
2023-01-23 14:17:13 +0100

    pick image

--- humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/MainActivity2.java
+++ humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/MainActivity2.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.provider.MediaStore;
@@ -24,12 +27,12 @@ public class MainActivity2 extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMain2Binding binding;
-    /**
+
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
     ImageView foto_gallery;
     private Object result;
-    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +41,7 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-        /**
+
         foto_gallery = (ImageView)findViewById(R.id.addImagen);
 
         foto_gallery.setOnClickListener(new View.OnClickListener() {
@@ -47,23 +50,34 @@ public class MainActivity2 extends AppCompatActivity {
                 openGallery();
             }
         });
-         */
+
     }
-    /*
+//Visto: https://es.stackoverflow.com/questions/41707/cargar-una-imagen-desde-la-galeria-android
+//       https://stackoverflow.com/questions/71082372/startactivityforresult-is-deprecated-im-trying-to-update-my-code
     private void openGallery(){
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
-        registerForActivityResult(gallery, PICK_IMAGE);
+        /**
+        ActivityResultLauncher<Intent> startActivityIntent = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        imageUri = gallery.getData();
+                        foto_gallery.setImageURI(imageUri);
+                    }
+                });
+         */
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
             imageUri = data.getData();
             foto_gallery.setImageURI(imageUri);
         }
     }
-
-     */
 }
+
+
