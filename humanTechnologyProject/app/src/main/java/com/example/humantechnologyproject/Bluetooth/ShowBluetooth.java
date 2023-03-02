534c7d18d42b1e086919942d17e642a7376b1313
AAusa <95350078+AAusa@users.noreply.github.com>
2023-03-02 12:17:39 +0100

    permisosBluetoothaMejorar

--- humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/Bluetooth/ShowBluetooth.java
+++ humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/Bluetooth/ShowBluetooth.java
@@ -1,10 +1,15 @@
 package com.example.humantechnologyproject.Bluetooth;
 
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +19,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import com.example.humantechnologyproject.Button;
 import com.example.humantechnologyproject.R;
 import com.example.humantechnologyproject.databinding.ActivityShowBluetoothBinding;
+import com.example.humantechnologyproject.db.DBButtons;
+
 import java.io.IOException;
 
 /*
@@ -28,6 +35,8 @@ public class ShowBluetooth extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityShowBluetoothBinding binding;
+    String strLetra;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,32 @@ public class ShowBluetooth extends AppCompatActivity {
         binding = ActivityShowBluetoothBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        strLetra = intent.getStringExtra("letra");
+        if (strLetra != null) {
+        }
+        //Poner el id q corresponde con las 4 letras que se reciben
+        DBButtons buttonId = new DBButtons(getBaseContext());
+        //aqui va el switch con las 4 opciones de letras y volvado a la variable id
+        int id = 0;
+        Button b = buttonId.buttonView(id);
+        if (id > 0) {
+            setFields(b);
+        }
+        moveTaskToBack(false);
+        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        am.moveTaskToFront(this.getTaskId(),0);
     }
 
     //volcar los datos de la BD a los campos
