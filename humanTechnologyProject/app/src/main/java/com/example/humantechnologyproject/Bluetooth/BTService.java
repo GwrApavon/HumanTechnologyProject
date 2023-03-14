219dd75b35ddf98927ca2f0c6b7ee6a8026fdb8d
AAusa <95350078+AAusa@users.noreply.github.com>
2023-03-14 12:29:08 +0100

    Update BTService.java

--- humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/Bluetooth/BTService.java
+++ humanTechnologyProject/app/src/main/java/com/example/humantechnologyproject/Bluetooth/BTService.java
@@ -63,7 +63,7 @@ public class BTService extends Service {
         // Crear una notificación que abra la portada de la aplicación al tocarla
         Intent notificationIntent = new Intent(this, MainActivity.class);
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
 
         // Para versiones de Android posteriores a Oreo crear la notificación con un canal propio
         Notification notification;
